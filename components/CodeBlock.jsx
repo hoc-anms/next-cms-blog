d94cb6e6dc61992f6b8db403597bbf80d37acac0
hoc-anms <phamvanhoc96@gmail.com>
2021-11-25 23:07:04 +0700

    restructure code

--- components/CodeBlock.jsx
+++ components/CodeBlock.jsx
@@ -5,9 +5,11 @@ import {dracula} from 'react-syntax-highlighter/dist/cjs/styles/prism';
 const CodeBlock = {
   code({node, inline, className, children, ...props}) {
     const match = /language-(\w+)/.exec(className || '')
+    const customStyle = {...dracula};
+    customStyle['pre[class*="language-"]']['background'] = '#011626 !important'
     return !inline && match ? (
-    <SyntaxHighlighter 
-      style={dracula} 
+    <SyntaxHighlighter
+      style={customStyle}
       language={match[1]} 
       PreTag="div" {...props}>
       {String(children).replace(/\n$/, '')}
