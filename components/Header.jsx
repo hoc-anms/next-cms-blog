c2b6f0ff0e12ac49f9cbf8c2c03c0989ddadce34
hoc-anms <phamvanhoc96@gmail.com>
2021-11-27 15:12:38 +0700

    update responsive header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/button-has-type */
 import React, { useState, useEffect } from 'react';
 
 import Link from 'next/link';
@@ -5,6 +7,11 @@ import { getCategories } from '../services';
 
 const Header = () => {
   const [categories, setCategories] = useState([]);
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
 
   useEffect(() => {
     getCategories().then((newCategories) => {
@@ -15,16 +22,50 @@ const Header = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
-        <div className="md:float-left block">
-          <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">VHP Blog</span>
-          </Link>
-        </div>
-        <div className="hidden md:float-left md:contents">
-          {categories.map((category, index) => (
-            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
-          ))}
-        </div>
+        <nav className="flex items-center flex-wrap">
+          <div className="md:float-left block">
+            <Link href="/">
+              <span className="cursor-pointer font-bold text-4xl text-white">VHP Blog</span>
+            </Link>
+          </div>
+          <button
+            className="inline-flex p-3 rounded lg:hidden text-white ml-auto hover:text-white outline-none"
+            onClick={handleClick}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+          <div
+            className={`${
+              active ? '' : 'hidden'
+            }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+          >
+            {categories.map((category, index) => (
+              <div key={index} className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
+                <Link key={index} href={`/category/${category.slug}`}>
+                  <a
+                    onClick={handleClick}
+                    className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:text-white"
+                  >
+                    {category.name}
+                  </a>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </nav>
       </div>
     </div>
   );
