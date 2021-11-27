8c0a481efae0b3b22a7d61cb0dcf8281849c4ffb
hoc-anms <phamvanhoc96@gmail.com>
2021-11-27 15:47:43 +0700

    update responsive header

--- components/Header.jsx
+++ components/Header.jsx
@@ -50,10 +50,10 @@ const Header = () => {
           <div
             className={`${
               active ? '' : 'hidden'
-            }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+            }   w-full lg:inline-flex lg:flex-grow lg:w-auto lg:ml-auto justify-end`}
           >
             {categories.map((category, index) => (
-              <div key={index} className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
+              <div key={index} className="lg:inline-flex lg:flex-row lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
                 <Link key={index} href={`/category/${category.slug}`}>
                   <a
                     onClick={handleClick}
